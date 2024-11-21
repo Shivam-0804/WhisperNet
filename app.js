f2dfe856c38acf31953b5c1be00b9479f01a42e2
Shivam Chaudhary <124515617+Shivam-0804@users.noreply.github.com>
2024-11-21 20:02:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ const cookieParser = require("cookie-parser");
 const userRoute = require("./routers/userRouter");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
-
-const User = require("./models/userModel");
 const authController = require("./controllers/authController");
-const message = require("./models/userModel");
 const chatRouter = require("./routers/chatRouter");
 
 const app = express();
@@ -18,6 +15,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
+
+// Routes
 app.use("/chat", authController.protect, chatRouter);
 app.use("/user/login", (req, res) => {
   res.render("login");
@@ -26,12 +25,18 @@ app.use("/user/signup", (req, res) => {
   res.render("signup");
 });
 app.use("/api", userRoute);
+
+// Root route
 app.get("/", (req, res) => {
   res.render("base");
 });
+
+// Catch-all for undefined routes
 app.all("*", (req, res, next) => {
-  next(new AppError(`Can't  find ${req.originalUrl} on this server!`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
+// Global error handler
 app.use(globalErrorHandler);
+
 module.exports = app;
