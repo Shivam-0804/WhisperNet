d545402cb490556bd4707fcaeedcaa91ee092c1e
Shivam Chaudhary <124515617+Shivam-0804@users.noreply.github.com>
2024-11-21 19:34:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,17 +9,16 @@ const Message = require("./models/chatModel");
 
 dotenv.config({ path: path.join(__dirname, ".env") });
 
-const app = require("./app");
-const server = express();
-const serv = http.createServer(server);
-const io = new Server(serv);
+const app = express(); // Changed this to avoid naming conflict
+const server = http.createServer(app); // Using app directly
+const io = new Server(server);
 
 const secure = require("./end_to_end");
 
-server.use(express.json());
-server.use(cookieParser());
-server.use("/", app);
+app.use(express.json());
+app.use(cookieParser());
 
+// MongoDB connection
 mongoose
   .connect(
     process.env.MONGO_URL.replace("<DB_PASSWORD>", process.env.DB_PASSWORD)
@@ -34,8 +33,9 @@ mongoose
 let messages = [];
 let chats = {};
 let onlineUsers = {};
+
 io.on("connection", (socket) => {
-  // console.log("client connected");
+  console.log("Client connected");
 
   socket.on("register", (userId) => {
     onlineUsers[userId] = socket.id;
@@ -47,50 +47,7 @@ io.on("connection", (socket) => {
       chats[chatId] = [];
     }
     socket.join(chatId);
-    // console.log(`Client joined chat ${chatId}`);
   });
-
-  // socket.on("message", async (message) => {
-  //   messages.push(message);
-  //   let encryptedMessage = await secure.encryptMessage(message.text);
-  //   if (!(await Message.findOne({ chatId: message.chatId }))) {
-  //     const messageDb = new Message({
-  //       chatId: message.chatId,
-  //       content: [
-  //         {
-  //           message: encryptedMessage,
-  //           senderId: message.senderId,
-  //           receiverId: message.receiverId,
-  //           timestamp: Date.now(),
-  //         },
-  //       ],
-  //     });
-  //     await messageDb.save();
-  //   } else {
-  //     const messageDb = await Message.findOneAndUpdate(
-  //       { chatId: message.chatId },
-  //       {
-  //         $push: {
-  //           content: {
-  //             message: encryptedMessage,
-  //             senderId: message.senderId,
-  //             receiverId: message.receiverId,
-  //           },
-  //         },
-  //       },
-  //       { new: true, useFindAndModify: false }
-  //     );
-  //   }
-  //   const msg = {
-  //     text: message.text,
-  //     date: Date.now(),
-  //   };
-  //   io.to(message.chatId).emit("message", {
-  //     message: msg,
-  //     senderId: message.senderId,
-  //   });
-  //   // console.log(`Received message: ${message.text}`);
-  // });
 
   socket.on("message", async (message) => {
     try {
@@ -100,10 +57,13 @@ io.on("connection", (socket) => {
         throw new Error("Message text must be a string");
       }
 
+      // Encrypt the message
       let encryptedMessage = await secure.encryptMessage(message.text);
       console.log("Encrypted message:", encryptedMessage);
 
-      if (!(await Message.findOne({ chatId: message.chatId }))) {
+      // Save message in the database
+      let existingMessage = await Message.findOne({ chatId: message.chatId });
+      if (!existingMessage) {
         const messageDb = new Message({
           chatId: message.chatId,
           content: [
@@ -132,6 +92,7 @@ io.on("connection", (socket) => {
         );
       }
 
+      // Emit the message to the chat room
       const msg = {
         text: message.text,
         date: Date.now(),
@@ -160,23 +121,28 @@ io.on("connection", (socket) => {
   });
 
   socket.on("history", async (chatId) => {
-    let data = await Message.findOne({ chatId: chatId.currentChat }).sort({
-      "content.timestamp": 1,
-    });
+    try {
+      let data = await Message.findOne({ chatId: chatId.currentChat }).sort({
+        "content.timestamp": 1,
+      });
 
-    if (!data) {
-      data = "undefined";
-    } else {
-      for (let i = 0; i < data.content.length; i++) {
-        data.content[i].message = await secure.decryptMessage(
-          data.content[i].message
-        );
+      if (!data) {
+        data = "undefined";
+      } else {
+        for (let i = 0; i < data.content.length; i++) {
+          data.content[i].message = await secure.decryptMessage(
+            data.content[i].message
+          );
+        }
       }
+
+      io.to(chatId.currentChat).emit("history", {
+        data: data.content,
+        userId: chatId.userId,
+      });
+    } catch (err) {
+      console.error("Error fetching history:", err.message);
     }
-    io.to(chatId.currentChat).emit("history", {
-      data: data.content,
-      userId: chatId.userId,
-    });
   });
 
   socket.on("disconnect", () => {
@@ -191,7 +157,8 @@ io.on("connection", (socket) => {
   });
 });
 
+// Ensure the application listens on the correct port for Render or local development
 const PORT = process.env.PORT || 3000;
-serv.listen(PORT, async () => {
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
